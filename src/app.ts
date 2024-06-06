9bed78faca99448921ad6277e96b143931196d76
swazza <swarup.karavadi@gmail.com>
2024-06-06 18:00:47 +0200

    chore: ran prettier on all code files

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import { config } from './init/config';
+import express from "express";
+import { config } from "./init/config";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 export function start() {
-    app.listen(config.port, () => {
-        console.log(`Server is running on port ${config.port}`);
-    });
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+  });
 }
-
