2b79fb11d6fb801881c75749d69dcc1a7305e85a
swazza <swarup.karavadi@gmail.com>
2024-06-09 00:41:02 +0200

    fix: added an endpoint to return the gitsha

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/metadata", (req, res) => {
+  res.json({
+    gitSHA: config.gitSHA,
+  });
+});
+
 export function start() {
   app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
